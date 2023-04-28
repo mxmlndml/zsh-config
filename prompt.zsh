8a01ef0c0fd325d11f8066e5d2ba69aaf7b48f8e
mxmlndml <42516330+MXMLNDML@users.noreply.github.com>
2023-04-28 22:37:50 +0200

    add prompt config

---
+++ prompt.zsh
@@ -0,0 +1,13 @@
+# git status
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+setopt prompt_subst
+zstyle ':vcs_info:git:*' formats '%F{247}on branch%f %B%F{208}%b%f'
+autoload -Uz compinit && compinit
+
+# prompt
+# username@machine in directory on branch branchname
+# % 
+NEWLINE=$'\n'
+PROMPT='${NEWLINE}%n@%m %F{247}in%f %B%F{39}%1~%f%b ${vcs_info_msg_0_}%b ${NEWLINE}%# '
